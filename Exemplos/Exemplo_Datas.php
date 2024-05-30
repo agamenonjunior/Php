2ae2c3610433f01014dba24466507d1e03977fcf
agamenon <agamenon_junior1@hotmail.com>
2024-05-29 21:20:27 -0300

    Adição de Exemplo Data

--- Exemplos/Exemplo_Datas.php
+++ Exemplos/Exemplo_Datas.php
@@ -1,3 +1,10 @@
 <?php
+require_once("../Funcoes/Funcoes.php");
+/**
+ * @author Agamenon Junior
+ * 
+ * Diferença entre 2 datas
+ */
+$dif = Diferenca_entre_datas("2024-05-28", "2024-05-29");
+echo $dif;
 
-?>
--- Funcoes/Funcoes.php
+++ Funcoes/Funcoes.php
@@ -4,8 +4,24 @@
  * Data Hoje 
  * @return date Data Atual
  */
-function Data_Hoje(){
+function Data_Hoje()
+{
     return date('d-m-Y');
 }
 
-?>
+
+/**
+ * Diferenca_entre_datas
+ *
+ * @param DateTime $data1 - Y-m-d
+ * @param DateTime $data2 - Y-m-d
+ * @return string - Quantidade de dias 
+ */
+function Diferenca_entre_datas($data1, $data2)
+{
+    $dt1 = new DateTime("$data1");
+    $dt2 = new DateTime("$data2");
+    $intervalo = $dt1->diff($dt2);
+    return $intervalo->format('%a dia(s)');
+}
+
