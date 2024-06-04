2c48d77b241cae3835036300d088026258a6edcf
agamenon <agamenon_junior1@hotmail.com>
2024-06-03 23:41:17 -0300

    Commit Inicial

--- Classes/Produtos.php
+++ Classes/Produtos.php
@@ -1,121 +1,128 @@
 <?php
+
+/**
+ * 
+ * @author Agamenon Junior 
+ * 
+ */
 class Produto
 {
-private $id;
-private $Nome;
-private $valor;
-private $imagem;
-private $quantidade;
-private $categoria;
+
+    private $id;
+    private $Nome;
+    private $valor;
+    private $imagem;
+    private $quantidade;
+    private $categoria;
 
 
-/**
- * Get the value of id
- */
-public function getId()
-{
-return $this->id;
+    /**
+     * Get the value of id
+     * @param int $id
+     * @return int id
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of Nome
+     */
+    public function getNome()
+    {
+        return $this->Nome;
+    }
+
+    /**
+     * Set the value of Nome
+     */
+    public function setNome($Nome): self
+    {
+        $this->Nome = $Nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of valor
+     */
+    public function getValor()
+    {
+        return $this->valor;
+    }
+
+    /**
+     * Set the value of valor
+     */
+    public function setValor($valor): self
+    {
+        $this->valor = $valor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of imagem
+     */
+    public function getImagem()
+    {
+        return $this->imagem;
+    }
+
+    /**
+     * Set the value of imagem
+     */
+    public function setImagem($imagem): self
+    {
+        $this->imagem = $imagem;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of quantidade
+     */
+    public function getQuantidade()
+    {
+        return $this->quantidade;
+    }
+
+    /**
+     * Set the value of quantidade
+     */
+    public function setQuantidade($quantidade): self
+    {
+        $this->quantidade = $quantidade;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of categoria
+     */
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    /**
+     * Set the value of categoria
+     */
+    public function setCategoria($categoria): self
+    {
+        $this->categoria = $categoria;
+
+        return $this;
+    }
 }
-
-/**
- * Set the value of id
- */
-public function setId($id): self
-{
-$this->id = $id;
-
-return $this;
-}
-
-/**
- * Get the value of Nome
- */
-public function getNome()
-{
-return $this->Nome;
-}
-
-/**
- * Set the value of Nome
- */
-public function setNome($Nome): self
-{
-$this->Nome = $Nome;
-
-return $this;
-}
-
-/**
- * Get the value of valor
- */
-public function getValor()
-{
-return $this->valor;
-}
-
-/**
- * Set the value of valor
- */
-public function setValor($valor): self
-{
-$this->valor = $valor;
-
-return $this;
-}
-
-/**
- * Get the value of imagem
- */
-public function getImagem()
-{
-return $this->imagem;
-}
-
-/**
- * Set the value of imagem
- */
-public function setImagem($imagem): self
-{
-$this->imagem = $imagem;
-
-return $this;
-}
-
-/**
- * Get the value of quantidade
- */
-public function getQuantidade()
-{
-return $this->quantidade;
-}
-
-/**
- * Set the value of quantidade
- */
-public function setQuantidade($quantidade): self
-{
-$this->quantidade = $quantidade;
-
-return $this;
-}
-
-/**
- * Get the value of categoria
- */
-public function getCategoria()
-{
-return $this->categoria;
-}
-
-/**
- * Set the value of categoria
- */
-public function setCategoria($categoria): self
-{
-$this->categoria = $categoria;
-
-return $this;
-}
-}
-
-?>
