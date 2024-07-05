41a5138eefe7e72113ec538dd2791f9a5b6dcfcd
agamenon <agamenon_junior1@hotmail.com>
2024-07-04 22:25:48 -0300

    refatorando classe

--- Classes/Upload.php
+++ Classes/Upload.php
@@ -5,8 +5,10 @@ class Upload{
  *  Classe Upload 
  */
 
+ private $id;
  private $documento;
  
+
  public function arquivo($documento){
     
  }
@@ -17,6 +19,42 @@ class Upload{
 
 
 
+
+ /**
+  * Get the value of id
+  */
+ public function getId()
+ {
+  return $this->id;
+ }
+
+ /**
+  * Set the value of id
+  */
+ public function setId($id): self
+ {
+  $this->id = $id;
+
+  return $this;
+ }
+
+ /**
+  * Get the value of documento
+  */
+ public function getDocumento()
+ {
+  return $this->documento;
+ }
+
+ /**
+  * Set the value of documento
+  */
+ public function setDocumento($documento): self
+ {
+  $this->documento = $documento;
+
+  return $this;
+ }
 }
 
 ?>
